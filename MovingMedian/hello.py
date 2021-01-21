b767fd2a03fe4e83e68972c5b57b102accef67d7
Julius Bacosa <juliusbacosa@gmail.com>
2021-01-21 15:14:50 +0800

    python

--- MovingMedian/hello.py
+++ MovingMedian/hello.py
@@ -1,5 +1,14 @@
-def main():
-    print('Hello')
 
+def recursive(i):
+    if i <= 10:
+       i += 1
+       print('hello ' + str(i))
+       recursive(i)
+    return
+
+def main():
+    i = 0
+    print('Hello')
+    recursive(i)
 
 main()    
