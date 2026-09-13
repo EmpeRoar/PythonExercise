256eaf9db4cf4f5ab0e915f9eae107434fd22223
Julius Bacosa <juliusbacosa@gmail.com>
2026-09-13 08:37:56 +0800

    ...

--- 1PythonSunday/dictionary.py
+++ 1PythonSunday/dictionary.py
@@ -5,3 +5,8 @@ print(phonebook)
 
 for name, number in phonebook.items():
     print(f"{name}: {number}")
+
+if "John" in phonebook:
+    print("John's number is:", phonebook["John"])  
+if "Alice" not in phonebook:
+    print("Alice is not in the phonebook.")    
