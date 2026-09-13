33a3c151c32ac943c8c622828018c5ad381f501f
Julius Bacosa <juliusbacosa@gmail.com>
2026-09-13 08:25:32 +0800

    ...

--- PythonSunday/strings.py
+++ PythonSunday/strings.py
@@ -3,3 +3,15 @@ print("Hello, %s!" % name)
 
 age = 23
 print("%s is %d years old." % (name, age))
+
+astring = "Hello world!"
+print(astring.index("o"))
+
+astring = "Hello world!"
+print(astring.count("l"))
+
+astring = "Hello world!"
+print(astring[3:7])
+
+astring = "Hello world!"
+print(astring[3:7:2])
